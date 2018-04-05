f965d217b25197868eea3c3a57e28be39ab7b2f4
Morgan Chorlton <morganchorlton3@gmail.com>
2018-04-05 21:52:01 +0100

    Initial commit Working bottom Nav with working recycler view

---
+++ app/src/main/java/com/vfestival/Artists.java
@@ -0,0 +1,35 @@
+package com.vfestival;
+
+
+public class Artists {
+    public static String[] title = new String[]{
+            "Test",
+            "Test 2",
+            "Test 3",
+            "Test",
+            "Test 2",
+            "Test 3",
+            "Test",
+            "Test 2",
+            "Test 3",
+            "Test",
+            "Test 2",
+            "Test 3",
+
+    };
+
+    public static  int[] imgPath = new int[]{
+            R.drawable.themartian,
+            R.drawable.thevigitarian,
+            R.drawable.thewildrobot,
+            R.drawable.themartian,
+            R.drawable.thevigitarian,
+            R.drawable.thewildrobot,
+            R.drawable.themartian,
+            R.drawable.thevigitarian,
+            R.drawable.thewildrobot,
+            R.drawable.themartian,
+            R.drawable.thevigitarian,
+            R.drawable.thewildrobot,
+    };
+}
---
+++ app/src/main/java/com/vfestival/MainActivity.java
@@ -0,0 +1,75 @@
+package com.vfestival;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+
+import com.vfestival.Fragments.HomeFragment;
+import com.vfestival.Fragments.LineUpFragment;
+import com.vfestival.Fragments.TicketsFragment;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    Fragment fragment = null;
+
+    public void switchToFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_view, fragment);
+        ft.commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_lineup:
+                    fragment = new LineUpFragment();
+                    switchToFragment(fragment);
+                    return true;
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    switchToFragment(fragment);
+                    return true;
+                case R.id.navigation_tickets:
+                    fragment = new TicketsFragment();
+                    switchToFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment = new HomeFragment();
+
+        if(savedInstanceState == null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_view, fragment);
+            ft.commit();
+        }
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_home);
+
+    }
+}
