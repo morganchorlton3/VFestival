cd63147b7993d43c9ac0d6df53805d57ac5b807e
Morgan Chorlton <morganchorlton3@gmail.com>
2018-04-09 23:23:17 +0100

    Working send SMS

--- app/src/main/java/com/vfestival/Fragments/HomeFragment.java
+++ app/src/main/java/com/vfestival/Fragments/HomeFragment.java
@@ -1,16 +1,31 @@
 package com.vfestival.Fragments;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.vfestival.R;
 
 public class HomeFragment extends Fragment {
+
+    final int SEND_SMS_PERMISSION_REQUEST_CODE =1;
+
+    Button sendText;
 
     @Nullable
     @Override
@@ -22,5 +37,41 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        sendText = view.findViewById(R.id.sendText);
+
+        sendText.setEnabled(false);
+        if(checkPermission(Manifest.permission.SEND_SMS)){
+            sendText.setEnabled(true);
+        }else {
+            ActivityCompat.requestPermissions((Activity) getContext(),
+                    new String[]{Manifest.permission.SEND_SMS}, SEND_SMS_PERMISSION_REQUEST_CODE);
+        }
+        sendText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onSend(v);
+            }
+        });
+    }
+    public void onSend(View v){
+        String phoneNumber = "5554";
+        String smsMessage = "This is a test message from VFest";
+
+        if(phoneNumber == null || phoneNumber.length() == 0 ||
+                smsMessage == null || smsMessage.length() == 0){
+            return;
+        }
+
+        if(checkPermission(Manifest.permission.SEND_SMS)){
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, smsMessage, null, null);
+            Toast.makeText(getContext(), "Message Sent!", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean checkPermission(String permission){
+        int check = ContextCompat.checkSelfPermission(getContext(), permission);
+        return (check == PackageManager.PERMISSION_GRANTED);
     }
 }
--- app/src/main/java/com/vfestival/Fragments/TicketsFragment.java
+++ app/src/main/java/com/vfestival/Fragments/TicketsFragment.java
@@ -4,6 +4,7 @@ package com.vfestival.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,5 +23,6 @@ public class TicketsFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
     }
 }
